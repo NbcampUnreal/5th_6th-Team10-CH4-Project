7acadb2ccb34fa82f0a2c83052f0d72a0c47e5b1
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-12-08 16:23:22 +0900

    (2025.12.08) ThirdPersonTemplate

---
+++ Team10_4Project/Source/Team10_4Project/Team10_4ProjectGameMode.h
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Team10_4ProjectGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a third person game
+ */
+UCLASS(abstract)
+class ATeam10_4ProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	/** Constructor */
+	ATeam10_4ProjectGameMode();
+};
+
+
+

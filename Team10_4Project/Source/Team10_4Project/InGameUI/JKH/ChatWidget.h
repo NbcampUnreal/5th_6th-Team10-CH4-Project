0d358ae89ade422cb2e0d182ae608785bc4c23cf
Jokimiddle <joki6180@gmail.com>
2025-12-12 15:19:13 +0900

    feat: ChatSubsystem & ChatWidget
    구체적인 로직을 구현했습니다

--- Team10_4Project/Source/Team10_4Project/InGameUI/JKH/ChatWidget.h
+++ Team10_4Project/Source/Team10_4Project/InGameUI/JKH/ChatWidget.h
@@ -3,9 +3,27 @@
 #include "Blueprint/UserWidget.h"
 #include "ChatWidget.generated.h"
 
+struct FChatMessage;
+class UEditableTextBox;
+class UVerticalBox;
+
 UCLASS()
 class TEAM10_4PROJECT_API UChatWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> ChatInput; // ChatInput
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UVerticalBox> ChatMessageList; // ChatMessageList
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	UFUNCTION()
+	void OnChatInputCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+	void ChatInputReady();
+	UFUNCTION()
+	void OnChatMessage(const FChatMessage& ChatMessage);
 };
